50af6fd02568989f5097dd96ec70abca9733001e
Florian Baugé <florian.bauge.etu@univ-lemans.fr>
2023-01-11 16:31:28 +0100

    2sd Commit

--- src/test/java/com/florian/TP1/ZooTest.java
+++ src/test/java/com/florian/TP1/ZooTest.java
@@ -4,9 +4,20 @@ import org.junit.jupiter.api.Test;
 
 public class ZooTest {
     @Test
-    public void testNombreMaxiVisiteur(){
+    public void testNombreMaxiVisiteur() throws LimiteVisiteurException {
         Zoo zoo=new Zoo();
         zoo.ajouterSecteur(TypeAnimal.Lion);
+        try {
+            for (int i = 0; i <16 ; i++) {
+                zoo.nouveauVisiteur();}
+        }
+        catch (LimiteVisiteurException e)
+
+
+
+        }
+
+
 
     }
 }
