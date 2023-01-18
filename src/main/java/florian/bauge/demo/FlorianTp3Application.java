1693f029cddd390ee02906022059f93d016ff541
florian <florian.bauge.etu@univ-lemans.fr>
2023-01-18 09:53:30 +0100

    first commit

---
+++ src/main/java/florian/bauge/demo/FlorianTp3Application.java
@@ -0,0 +1,13 @@
+package florian.bauge.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlorianTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlorianTp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/florian/bauge/demo/FlorianTp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package florian.bauge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlorianTp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
